0a0204c97ea3d0ccc7673ab14893cb9c0f7575c9
mutita boonkum <61160018@go.buu.ac.th>
2021-03-04 19:33:01 +0700

    POST /books to store book data into array

---
+++ index.js
@@ -0,0 +1,35 @@
+const express = require('express')
+const app = express()
+app.use(express.json())
+let books = []
+
+
+
+app.post('/books',(req, res) => {
+    //input
+    let newTitle = req.body.title
+    let newPrice = req.body.price
+    let newUnit = req.body.unit 
+    let newIsbn = req.body.isbn
+    let newImage = req.body.image
+
+    // key
+    let newBook = {
+        title: newTitle,
+        price: newPrice,
+        unit: newUnit,
+        isbn: newIsbn,
+        image: newImage
+       
+    }
+    let bookID = 0
+
+    //process
+    books.push(newBook)
+    bookID = books.length-1
+    //output
+    res.status(201).json(bookID)
+})
+
+const port = 3000
+app.listen(port, () => console.log(`Server started at ${port}`))
